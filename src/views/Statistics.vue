3de73f1c9dce5f023910b98aba0cafa1b7c644de
ygc <937601471@qq.com>
2021-07-04 23:58:46 +0800

    重复三次布局

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Statistics.vue
+  <div class="nav-warpper">
+    <div class="content">
+      Statistics.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-warpper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  /*尽量把所有的高度都给 content*/
+  flex-grow: 1;
+}
 </style>
