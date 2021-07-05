e1feaf2ee9b8de2c1dc3734485cbd74087425683
ygc <937601471@qq.com>
2021-07-05 21:19:46 +0800

    页面引入 svg
    1. iconfont 下载 svg 文件
    2. 安装 svg-sprite-loader 组件
    3. 引入 svg 资源
    4. vue.config.js修改，页面成功导入 svg symbol 标签
    
    注意：步骤4中console.log(x);很关键，没有这个svg中不会有对应的 symbol 标签，也就没法使用 use 标签关联显示

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#money"></use>
+      </svg>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#label"></use>
+      </svg>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"></use>
+      </svg>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/money.svg';
+import y from '@/assets/icons/label.svg';
+import z from '@/assets/icons/statistics.svg';
+
+console.log(x);
+console.log(y);
+console.log(z);
+
 export default {
   name: 'Nav'
 };
