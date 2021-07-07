f0009518388780d6028a09e8aa9f17da13291b5d
ygc <937601471@qq.com>
2021-07-07 21:06:21 +0800

    使用 js 写一个组件的业务逻辑
    为了复习 Vue 的 js 写法，这里拿 Money 页面的消费类型作为案例

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <!-- 如果是 false，vue 会智能地忽略掉 -->
+      <!-- 等价于: type === '-' ? 'selected' : ''-->
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
 export default {
-  name: "Types"
+  name: 'Types',
+  props: ['xxx'],
+  data() {
+    return {
+      type: '-'// '-'表示支出，'+'表示收入
+    }
+  },
+  mounted() {
+    console.log(this.xxx)
+  },
+  methods: {
+    selectType(type) {// type 只能是 '-' 和 '+' 中的一个
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown')
+      }
+      this.type = type
+    }
+  }
 }
 </script>
 
