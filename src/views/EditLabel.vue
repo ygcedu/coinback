61fdcc85b3f7ab4f1b975139082a17e52a0704f0
ygc <937601471@qq.com>
2021-07-11 01:18:45 +0800

    EditLabel 组件路由添加 id 参数
    判断 id 是否存在，不存在跳转到 404 页面

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,10 +7,22 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
+import tagListModel from '@/models/tagListModel';
 
 @Component
 export default class EditLabel extends Vue {
-
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter(t => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      // 用 replace 不要用 push，没法回退
+      this.$router.replace('/404');
+    }
+  }
 }
 </script>
 
