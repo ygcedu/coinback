574c0e3bb56d444f756627d6e366a2b59966b09c
ygc <937601471@qq.com>
2021-07-11 00:45:04 +0800

    create 返回类型改为联合类型
    方案说明：基于 typescript 的联合类型解决返回 string 存在的问题

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,10 +1,11 @@
+const localStorageKeyName = 'tagList';
+
 type TagListModel = {
   data: string[]
   fetch: () => string[]
-  create: (name: string) => { code: number, message: string } // 'success' 表示成功，'duplicated' 表示 name 重复
+  create: (name: string) => 'success' | 'duplicated' // 联合类型
   save: () => void
 }
-const localStorageKeyName = 'tagList';
 
 const tagListModel: TagListModel = {
   data: [],
@@ -14,11 +15,11 @@ const tagListModel: TagListModel = {
   },
   create(name: string) {
     if (this.data.indexOf(name) >= 0) {
-      return {code: 1, message: 'duplicated'};
+      return 'duplicated';
     }
     this.data.push(name);
     this.save();
-    return {code: 0, message: 'success'};
+    return 'success';
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -26,12 +26,11 @@ export default class Labels extends Vue {
   createTag() {
     const name = window.prompt('请输入标签名');
     if (name) {
-      try {
-        tagListModel.create(name);
-      } catch (error) {
-        if (error.message === 'duplicate') {
-          window.alert('标签名重复');
-        }
+      const message = tagListModel.create(name);
+      if (message === 'duplicated') {
+        window.alert('标签名重复了');
+      } else if (message === 'success') {
+        window.alert('添加成功');
       }
     }
   }
