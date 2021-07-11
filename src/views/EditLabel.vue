4b6982e802f18c7fab00ca3cc302f36795f8e206
ygc <937601471@qq.com>
2021-07-11 10:05:21 +0800

    Notes 组件重命名为 FormItem

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -4,7 +4,7 @@
       <Icon name="left"/>
       <span>编辑标签</span>
     </div>
-    <Notes field-name="标签名" placeholder="请输入标签名"/>
+    <FormItem field-name="标签名" placeholder="请输入标签名"/>
   </layout>
 </template>
 
@@ -12,9 +12,9 @@
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
-import Notes from '@/components/Money/Notes.vue';
+import FormItem from '@/components/Money/FormItem.vue';
 @Component({
-  components: {Notes}
+  components: {FormItem}
 })
 export default class EditLabel extends Vue {
   created() {
