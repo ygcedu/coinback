f1f32a7a92e4ff65ac5dfb852510f184c2a5fcfa
ygc <937601471@qq.com>
2021-07-12 21:52:12 +0800

    解决 id 生成问题

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,3 +1,5 @@
+import createId from '@/lib/idCreator';
+
 const localStorageKeyName = 'tagList';
 
 type Tag = {
@@ -26,7 +28,8 @@ const tagListModel: TagListModel = {
     if (names.indexOf(name) >= 0) {
       return 'duplicated';
     }
-    this.data.push({id: name, name: name});
+    const id = createId().toString();
+    this.data.push({id, name: name});
     this.save();
     return 'success';
   },
