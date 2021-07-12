137e64c18989b754ebe4bdb3974595f54f2fcf84
ygc <937601471@qq.com>
2021-07-12 22:36:25 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -3,15 +3,18 @@ import {RecordItem} from '@/custom';
 const localStorageKeyName = 'recordList';
 
 const recordListModel = {
+  data: [] as RecordItem[],
   clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
   fetch() {
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     // as 强行指定返回数据类型为 RecordItem[]
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.data;
   },
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKeyName,
+      JSON.stringify(this.data));
   }
 };
 
