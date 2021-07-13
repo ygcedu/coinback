36bc8c8076b2c3be821cfde5f3ce2e10bcaf2dbd
ygc <937601471@qq.com>
2021-07-13 22:56:27 +0800

    把 recordListModel.ts 内容全部移植到 recordStore.ts 中

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,9 +1,28 @@
-import recordListModel from '@/models/recordListModel';
 import {RecordItem} from '@/custom';
+import clone from '@/lib/clone';
 
+const localStorageKeyName = 'recordList';
+let data: RecordItem[] | undefined = undefined;
+
+function fetch() {
+  data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+  // as 强行指定返回数据类型为 RecordItem[]
+  return data;
+}
+
+function save() {
+  window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+}
 
 export default {
   // record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
+  fetch: fetch,
+  save: save,
+  recordList: fetch(),
+  createRecord: (record: RecordItem) => {
+    const record2: RecordItem = clone(record);
+    record2.createAt = new Date();
+    data && data.push(record2);
+    save();
+  }
 };
