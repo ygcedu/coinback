a35fad27d29b5e4e6533ec61fc5c42aac43f01ee
ygc <937601471@qq.com>
2021-07-13 23:40:45 +0800

    使用全局数据改造 Tag.vue
    1. 使用全局 store 获取数据，代替从父组件 Money.vue 传 prop 数据
    2. 解决记账页面新增标签不显示标签文字 bug

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,7 +4,7 @@
       <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag.id"
+      <li v-for="tag in tagList" :key="tag.id"
           :class="{selected: selectedTags.indexOf(tag)>=0}"
           @click="toggle(tag)">{{ tag.name }}
       </li>
@@ -15,11 +15,11 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
+import store from '@/store/index2';
 
 @Component
 export default class Tags extends Vue {
-  // @Prop(Array) tags: string[] = [];
-  @Prop() readonly dataSource: string[] | undefined;
+  tagList = store.fetchTags();
   selectedTags: string[] = [];
 
   // 开关
@@ -35,14 +35,10 @@ export default class Tags extends Vue {
 
   create() {
     const name = window.prompt('请输入标签名');
-    if (name === '') {
-      window.alert('标签名不能为空');
-    } else {
-      // this.dataSource?.push(name!);// 不能改外部数据！
-      if (this.dataSource) {
-        this.$emit('update:dataSource', [...this.dataSource, name]);
-      }
+    if (!name) {
+      return window.alert('标签名不能为空');
     }
+    store.createTag(name);
   }
 }
 </script>
