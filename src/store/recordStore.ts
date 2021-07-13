e69e8f5d07e3afa86bb3006814b243692366911e
ygc <937601471@qq.com>
2021-07-13 23:01:32 +0800

    重构 recordStore.ts

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -4,25 +4,25 @@ import clone from '@/lib/clone';
 const localStorageKeyName = 'recordList';
 let data: RecordItem[] | undefined = undefined;
 
-function fetch() {
-  data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
-  // as 强行指定返回数据类型为 RecordItem[]
-  return data;
-}
-
-function save() {
-  window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
-}
-
-export default {
+const recordStore = {
   // record store
-  fetch: fetch,
-  save: save,
-  recordList: fetch(),
+  fetch() {
+    data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    // as 强行指定返回数据类型为 RecordItem[]
+    return data;
+  },
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  },
+  recordList: data,
   createRecord: (record: RecordItem) => {
     const record2: RecordItem = clone(record);
     record2.createAt = new Date();
     data && data.push(record2);
-    save();
+    recordStore.save();
   }
 };
+
+recordStore.fetch();
+
+export default recordStore;
