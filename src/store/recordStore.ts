29e45312ad6893c1afee088c44ab7b2634094fa6
ygc <937601471@qq.com>
2021-07-13 23:03:44 +0800

    recordStore.ts 方法重命名

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -5,24 +5,24 @@ const localStorageKeyName = 'recordList';
 let data: RecordItem[] | undefined = undefined;
 
 const recordStore = {
+  recordList: data,
   // record store
-  fetch() {
+  fetchRecords() {
     data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     // as 强行指定返回数据类型为 RecordItem[]
     return data;
   },
-  save() {
+  saveRecord() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
   },
-  recordList: data,
   createRecord: (record: RecordItem) => {
     const record2: RecordItem = clone(record);
     record2.createAt = new Date();
     data && data.push(record2);
-    recordStore.save();
+    recordStore.saveRecord();
   }
 };
 
-recordStore.fetch();
+recordStore.fetchRecords();
 
 export default recordStore;
