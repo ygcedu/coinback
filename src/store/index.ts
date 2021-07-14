c3657947cf5c888a44eff8a51806089a473c8e4a
ygc <937601471@qq.com>
2021-07-14 23:14:48 +0800

    初识 Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,23 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const store = new Vuex.Store({
+  state: {// data
+    count: 0,
+    n: 0,
   },
-  mutations: {
+  mutations: {// methods
+    increment(state, n: number) {
+      state.count += n;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
+
+console.log(store.state.count);// 0
+store.commit('increment', 10);// +10
+console.log(store.state.count);// 1
+
+export default store;
+
