8f0b010ee3d01ffc98cf580313316b872f1e5c23
ygc <937601471@qq.com>
2021-07-16 00:32:54 +0800

    删除 Types.vue
    以后所有选项卡都用更加通用的 Tabs.vue 取代

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,7 +13,6 @@
 
 <script lang="ts">
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
@@ -23,7 +22,7 @@ import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tabs, Tags, FormItem, Types, NumberPad},
+  components: {Tabs, Tags, FormItem, NumberPad},
 })
 export default class Money extends Vue {
   record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -27,12 +27,11 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import Types from '@/components/Money/Types.vue';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
 
-@Component({components: {Tabs, Types},})
+@Component({components: {Tabs},})
 export default class Statistics extends Vue {
   type = '-';
   interval = 'day';
