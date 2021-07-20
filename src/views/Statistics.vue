e4825ad827f56de1b5cd99f1b490a65d6c4751ad
ygc <937601471@qq.com>
2021-07-20 22:11:49 +0800

    完成列表的 CSS
    发现 bug：备注很长时，如果是汉字会自动换行，如果是英文则会显示水平滚动条

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,25 +2,47 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" height="48px"/>
-    <div>
-      type:{{ type }}
-      <br/>
-      interval:{{ interval }}
-    </div>
-    <div>
-      <ol>
-        <li v-for="(group,index) in result" :key="index">
-          <h3>{{ group.title }}</h3>
-          <ol>
-            <li v-for="item in group.items" :key="item.id">
-              {{ item.amount }} {{ item.createdAt }}
-            </li>
-          </ol>
-        </li>
-      </ol>
-    </div>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <!-- fixme: 备注是很长的英文时会显示水平滚动条 -->
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
+
+<style scoped lang="scss">
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  @extend %item;
+  background: white;
+}
+
+.notes {
+  margin-right: auto; // 让备注靠左一些
+  margin-left: 16px;
+  color: #999;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -28,10 +50,14 @@ import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
-import {RecordItem, RootState} from '@/custom';
+import {RecordItem, RootState, Tag} from '@/custom';
 
 @Component({components: {Tabs},})
 export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
