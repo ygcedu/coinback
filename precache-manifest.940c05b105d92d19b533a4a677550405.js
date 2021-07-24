1b415458e423ca81b70c792776f06f8ba4ae08cb
ygcedu <937601471@qq.com>
2021-07-24 22:37:30 +0800

    提交 dist 文件夹到 master 分支

---
+++ precache-manifest.940c05b105d92d19b533a4a677550405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48932c4521e776700ea0",
+    "url": "/coinback/css/app.720b1dae.css"
+  },
+  {
+    "revision": "f859bb8e78cf85ce50ed7d0e94c2b48f",
+    "url": "/coinback/index.html"
+  },
+  {
+    "revision": "48932c4521e776700ea0",
+    "url": "/coinback/js/app.0a49e65d.js"
+  },
+  {
+    "revision": "92dcfe3a54fd9a8d520a",
+    "url": "/coinback/js/chunk-vendors.aecca5f9.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/coinback/manifest.json"
+  },
+  {
+    "revision": "5da63f5e76930b8251fb019f26761f58",
+    "url": "/coinback/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/coinback/robots.txt"
+  }
+]);
